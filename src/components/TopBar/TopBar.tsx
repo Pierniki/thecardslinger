c4b2667c6796e95858ed91bd99e5a9515e88c8a5
Pierniki <jjdroid6969@gmail.com>
2022-04-26 01:43:36 +0200

    style: restyle cards, minor topbar sprite changes, change fonts

--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -4,29 +4,31 @@ import { MoneyDisplay } from './MoneyDisplay'
 
 interface Props {}
 
+const topBarPath = '/assets/top-bar'
+
 export const TopBar: React.FC<Props> = () => {
   const money = useAppSelector((state) => state.player.money)
 
   return (
-    <div className="w-full flex justify-center overflow-x-hidden">
+    <div className="w-full flex justify-center overflow-hidden h-[80px]">
       <div
         className="min-w-[60px] mb-ext:block hidden bg-right bg-no-repeat bg-cover h-[80px]"
         style={{
-          backgroundImage: 'url(/assets/corner-left.png)',
+          backgroundImage: `url(${topBarPath}/corner-left.png)`,
           backgroundSize: 'auto 100%',
         }}
       />
       <div
         className="w-full mb-ext:block hidden bg-right bg-cover h-[56px]"
         style={{
-          backgroundImage: 'url(/assets/tile-top-left.png)',
+          backgroundImage: `url(${topBarPath}/tile-top-left.png)`,
           backgroundSize: 'auto 100%',
         }}
       />
       <div
         className="mb-ext:min-w-[450px] max-w-[450px] w-full h-[60px] flex justify-between items-start gap-8 px-5 pb-[18px] pt-[14px]  relative bg-top bg-no-repeat"
         style={{
-          backgroundImage: 'url(/assets/top-bg2.png)',
+          backgroundImage: `url(${topBarPath}/top-bg2.png)`,
           backgroundSize: '100% 100%',
         }}
       >
@@ -36,14 +38,14 @@ export const TopBar: React.FC<Props> = () => {
       <div
         className="w-full mb-ext:block hidden bg-right  bg-cover h-[56px]"
         style={{
-          backgroundImage: 'url(/assets/tile-top-right.png)',
+          backgroundImage: `url(${topBarPath}/tile-top-right.png)`,
           backgroundSize: 'auto 100%',
         }}
       />
       <div
         className="min-w-[60px] mb-ext:block hidden bg-left bg-no-repeat bg-cover  h-[80px]"
         style={{
-          backgroundImage: 'url(/assets/corner-right.png)',
+          backgroundImage: `url(${topBarPath}/corner-right.png)`,
           backgroundSize: 'auto 100%',
         }}
       />
